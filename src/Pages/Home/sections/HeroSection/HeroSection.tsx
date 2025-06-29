54c95abd19bf68dfbd8259cc32cf7f29e102feb5
buja23 <jooj.soares.227@gmail.com>
2025-06-29 17:35:35 -0300

    "hero"

--- src/Pages/Home/sections/HeroSection/HeroSection.tsx
+++ src/Pages/Home/sections/HeroSection/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { Box, Container, Grid, Typography, styled } from "@mui/material"
-import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground"
+import AnimatedBackground from "../../../../components/AnimatedBackground/AnimatedBackground";
 import Typewriter from "../../../../components/Typewriter/Typewriter"
-import Avatar from "../../../../assets/images/avatar.jpg"
+import Avatar from "../../../../assets/images/avatar.png"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
@@ -11,7 +11,7 @@ const HeroSection: React.FC = () => {
 
     const StyledImg = styled("img")(({ theme }) => ({
         width: "80%",
-        border: `1px solid ${theme.palette.primary.contrastText}`,
+        border: `1px solid ${theme.palette.secondary.contrastText}`,
         borderRadius: "50%",
         position: "relative"
     }));
@@ -58,14 +58,25 @@ const HeroSection: React.FC = () => {
 
     return (
         <>
-            <StyledHero>
-                <Container maxWidth="lg">
-                    <Grid container spacing={2}>
+            <StyledHero style={{ position: "relative", overflow: "hidden" }}>
+                {/* Background animado ocupa toda a área do HeroSection, mas não passa dele */}
+                <Box
+                    sx={{
+                        position: "absolute",
+                        inset: 0,
+                        width: "100%",
+                        height: "100%",
+                        zIndex: 0,
+                        pointerEvents: "none",
+                    }}
+                >
+                    <AnimatedBackground />
+                </Box>
+                {/* Conteúdo principal acima */}
+                <Container maxWidth="lg" sx={{ position: "relative", zIndex: 1 }}>
+                    <Grid container spacing={2} sx={{ position: "relative", zIndex: 1 }}>
                         <Grid item xs={12} md={5}>
                             <Box position="relative" pb={3}>
-                                <Box width={"150%"} position="absolute" top={-100} right={0}>
-                                    <AnimatedBackground />
-                                </Box>
                                 <Box textAlign="center">
                                     <StyledImg src={Avatar} />
                                 </Box>
@@ -73,16 +84,16 @@ const HeroSection: React.FC = () => {
                         </Grid>
                         <Grid item xs={12} md={7}>
                             <Typography color="primary.contrastText" variant="h1" pb={2} textAlign="center">
-                                Adriana Saty
+                                Victor Azambuja
                             </Typography>
-                            <Typewriter text="I'm a Software Engineer" delay={120} variant="h2" color="primary.contrastText" />
+                            <Typewriter text="Estudante de Análise e Desenvolvimento de Sistemas" delay={120} variant="h2" color="primary.contrastText" />
                             <Box mt={3}>
                                 <Grid container spacing={3} display="flex" justifyContent="center">
                                     <Grid item xs={10} md={4}>
                                         <StyledButton onClick={() => handleDownload()}>
                                             <DownloadIcon />
                                             <Typography>
-                                                Download CV
+                                                Baixar Curriculo
                                             </Typography>
                                         </StyledButton>
                                     </Grid>
@@ -90,7 +101,7 @@ const HeroSection: React.FC = () => {
                                         <StyledButton onClick={() => handleEmail()}>
                                             <EmailIcon />
                                             <Typography>
-                                                Contact me
+                                                Contatos
                                             </Typography>
                                         </StyledButton>
                                     </Grid>
